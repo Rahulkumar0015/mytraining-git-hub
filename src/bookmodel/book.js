ae802e55021dada23b42b03b7f3671f82e39afb1
RahulKumar0015 <merahulkumardhiraj@gmail.com>
2022-08-16 13:25:58 +0530

    add bookmodel

---
+++ src/bookmodel/book.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require("mongoose");
+
+const bookSchema =new mongoose.Schema({
+          bookName:String,
+          authorName:String,
+          category:String,
+          year:Number
+}
+,{timestamps:true}
+)
+module.exports=mongoose.model('bookName',bookSchema)
